c371cfce6ebb940da5357863eea0c10f89c88f5a
loukeira <markimsaraiva@gmail.com>
2019-02-13 00:17:47 -0200

    ok

--- PROJECT/data/actions/scripts/bonus.lua
+++ PROJECT/data/actions/scripts/bonus.lua
@@ -7,6 +7,9 @@ function onUse(cid, item, frompos, item2, topos)
    		if queststatus == -1 then
    			doPlayerSendTextMessage(cid,22,"Voce recebeu 5kk de bonus!.")
    			doPlayerAddItem(cid,2157,5)
+                        doPlayerAddItem(cid,7704,1)
+            doPlayerSendTextMessage(cid,22,"O papel é o item pra usar e ganhar 3 dias de autoloot! o npc vai ser adicionado em breve! NAO JOGUE FORA E USE LOGO!")
+
    			setPlayerStorageValue(cid,1604,1)
    		else
    			doPlayerSendTextMessage(cid,22,"Voce ja pegou o bonus")
